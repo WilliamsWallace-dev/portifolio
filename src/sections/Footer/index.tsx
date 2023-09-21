f02f9706091476e681b10a513635a4198c7eca1a
Williams Wallace <wwallace.dev@gmail.com>
2023-09-21 17:34:30 -0300

    ScrollToTopButton has been added and starsMov2's animation has been removed

--- src/sections/Footer/index.tsx
+++ src/sections/Footer/index.tsx
@@ -3,12 +3,13 @@ import IconGitHub  from "../../assets/Icon/iconGitHub.svg"
 import IconLinkedIn from "../../assets/Icon/IconLinkedIn.svg"
 import IconInstagram from "../../assets/Icon/IconInstagram.svg"
 import { Link, Outlet } from "react-router-dom"
+import UpArrow from "../../assets/Icon/IconArrowList.svg"
 
 export const Footer = ()=>{
     return(
         <>
             <Outlet></Outlet>
-            <section className="footerContainer">
+            <section className="footerContainer relative">
                 <p className="p2 mr-1">Todos os direitos reservados®</p>
                 <p className="p2 ml-1">Portifólio Williams Wallace ® 2023</p>
                 <div className="flex-center">
@@ -18,6 +19,7 @@ export const Footer = ()=>{
                     <Link to="https://www.linkedin.com/in/williams-wallace" target="_blank"><div className=" px-1 icon-svg"><img src={IconLinkedIn} alt="" /></div></Link>
                     <Link to="https://github.com/WilliamsWallace-dev" target="_blank"><div className=" px-1 icon-svg"><img src={IconGitHub} alt="" /></div></Link>
                 </div>
+                <button className="ScrollTotopButton" data-aos-anchor-placement="top-top" data-aos-anchor=".ContactSectionContent" data-aos-delay="100" data-aos-duration="1000" data-aos="fade-up"  onClick={()=>window.scrollTo(0,0)}><img src={UpArrow} alt="" /></button>
                 
             </section>
         </>
