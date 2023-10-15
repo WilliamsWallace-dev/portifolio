ce2ddd5d439f942431c78d27463148837e390ce2
Williams Wallace <wwallace.dev@gmail.com>
2023-10-14 23:46:41 -0300

    CV has been updated

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -8,13 +8,13 @@ export const Header = ()=>{
             <section className="header ">
                 <h4 className="logo" data-aos-delay="0" data-aos-duration="1500" data-aos="fade-up">Wwallace.Dev</h4>
                 <div className="flex-center" data-aos-delay="100" data-aos-duration="1500" data-aos="fade-up">
-                <Link to="https://drive.google.com/file/d/11KDiSmedhvDCBcIDy-8TIgwB3BY-fLFU/view?usp=sharing" target="_blank"><button className="btn-2 mr-3">Download CV</button></Link>
+                <Link to="https://drive.google.com/file/d/1gSm2l418l4tzp5YMe2ibSDOnm4QX6e8T/view?usp=sharing" target="_blank"><button className="btn-2 mr-3">Download CV</button></Link>
                 <a href="mailto:wwallace.dev@gmail.com?subject=Contato via Portifólio : "><button className="btn-1">Entrar em contato</button></a>
                 </div>
             </section>
             <section className="headerMobile ">
                 <div className="flex-center flex-between w-100" data-aos-delay="0" data-aos-duration="1500" data-aos="fade-up">
-                    <Link to="https://drive.google.com/file/d/11KDiSmedhvDCBcIDy-8TIgwB3BY-fLFU/view?usp=sharing" target="_blank"><button className="btn-2 mr-3 ">Download CV</button></Link>
+                    <Link to="https://drive.google.com/file/d/1gSm2l418l4tzp5YMe2ibSDOnm4QX6e8T/view?usp=sharing" target="_blank"><button className="btn-2 mr-3 ">Download CV</button></Link>
                     <button className="btn-1 ">Entrar em contato</button>
                 </div>
             </section>
