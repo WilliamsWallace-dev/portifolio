8e8200f110be390ef70b0ffb390be3807d7611d9
Williams Wallace <wwallace.dev@gmail.com>
2023-11-03 16:39:54 -0300

    CV has been updated

--- src/sections/ContactSection/index.tsx
+++ src/sections/ContactSection/index.tsx
@@ -12,7 +12,7 @@ export const ContactSection = ()=>{
                     <div data-aos-delay="0" data-aos-duration="1500" data-aos="fade-up" className="profileImage mb-2"><img className="profileImage" src={profileImage} alt="" /></div>
                     <h5 className="mb-1" data-aos-delay="100" data-aos-duration="1500" data-aos="fade-up">Vamos nos conectar !</h5>
                     <h4 data-aos-delay="200" data-aos-duration="1500" data-aos="fade-up">Gostaria de apresentar sua proposta ? </h4>
-                    <h4 className="email" data-aos-delay="300" data-aos-duration="1500" data-aos="fade-up">wwallace.dev@gmail.com</h4>
+                    <a href="mailto:wwallace.dev@gmail.com?subject=Contato via Portifólio : "><h4 className="email" data-aos-delay="300" data-aos-duration="1500" data-aos="fade-up">wwallace.dev@gmail.com</h4></a>
                 </section>
             </div>
             
